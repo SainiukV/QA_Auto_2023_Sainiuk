15074ab8e09dc085699ffa0ce53afe4066800885
DESKTOP-4CNLH54\38067 <sainiuk.v@gmail.com>
2023-08-06 18:38:42 +0300

    Project tasks

---
+++ modules/api/clients/github.py
@@ -0,0 +1,24 @@
+import requests
+
+
+class GitHub:
+    def get_user(self, username):
+        r = requests.get(f'https://api.github.com/users/{username}')
+        body = r.json()
+
+        return body
+
+    def search_repo(self, name):
+        r = requests.get('https://api.github.com/search/repositories',
+                          params={'q':name})
+        
+        body = r.json()
+
+        return body
+
+    def list_commits(self, owner, repo):
+        r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
+        body = r.json()
+
+        return body
+   
